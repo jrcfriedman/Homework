cc1c057595728dd10634e6eb43a3d7378f1b3c48
Jack Friedman <jrcfriedman@gmail.com>
2019-01-21 21:30:26 -0500

    draft edits

--- Final Project.R
+++ Final Project.R
@@ -1,13 +1,16 @@
 
 
+rm(list=ls())
 
+library(dplyr)
 
+# Reading in data
+vdem.full <- read.csv("~/Desktop/data_2018F/V-Dem-CY-Core-v8.csv")
 
-vdem8.full <- read.csv("~/Desktop/data_2018F/V-Dem-CY-Core-v8.csv")
-
+reb<-read.dta("http://willreed.org/concessions_elections.dta")
 
 # Subsetting V-dem data
-vdem <- subset(vdem8.full, year > 1959 & year < 2000, 
+vdem <- subset(vdem.full, year > 1959 & year < 2000, 
                select = c(country_name, country_text_id, country_id, year, v2x_polyarchy))
 
 
@@ -16,34 +19,23 @@ vdem <- vdem %>%
   group_by(country_id) %>%
   mutate(diff = v2x_polyarchy - lag(v2x_polyarchy))
 
-# New diff column for more decimals
-vdem$diff2 <- 0
-vdem[,"diff2"] <- round(vdem[,'diff'],10)
-
-
-# New variable for basic 0.01 threshold
-#  vdem <- vdem %>% 
-#    group_by(country_id) %>%
-#    mutate(decline2 = case_when(diff <= -0.01 ~ 1,
-#                               TRUE ~ 0))
-
 # New variable: 1 = decline greater/equal to .01, 2 = incline greater/equal to .02, 0 = all else
 vdem <- vdem %>% 
   group_by(country_id) %>%
   mutate(decline = case_when(diff <= -0.01 ~ 1,
                              diff >= 0.02 ~ 2,
                              TRUE ~ 0))
-         
-# Not quite right
+
+# Loop for creating episodes
 vdem$episode <- vdem$decline
 for (i in 2:nrow(vdem)) {
-  next.three <- vdem$episode[i:(i + 3)]
+  next.three <- vdem$decline[i:(i + 3)]
   if (vdem$episode[i] == 0) {
     if (vdem$episode[i - 1] == 1 & any(next.three == 1)) {
       vdem$episode[i] <- 1
       if (vdem$episode[i - 1] == 1 & any(next.three == 1)) {
         vdem$episode[i] <- 1
-        if ((vdem$decline[i] == 0) & (vdem$decline[i + 1] == 2)) {
+        if ((vdem$decline[i] == 0) & (((vdem$decline[i + 1] == 2 | vdem$decline[i + 2] == 2) & (vdem$decline[i + 1] != 1) ) ) ) {
           vdem$episode[i] <- 0
         }
       }
@@ -51,161 +43,120 @@ for (i in 2:nrow(vdem)) {
   }
 }
 
-# Think this is right, but need to check
-vdem$episode2 <- vdem$decline
+# Replaces 1's with 0's for first year per country
+vdem$episode[is.na(vdem$diff)] <- 99
+
+# Replace 2's with 0's
+vdem$episode[vdem$episode == 2] <- 0
+
+
+
+## Episode difference lag 1
+vdem$ep.diff <- 0
 for (i in 2:nrow(vdem)) {
-  next.three <- vdem$decline[i:(i + 3)]
-  if (vdem$episode2[i] == 0) {
-    if (vdem$episode2[i - 1] == 1 & any(next.three == 1)) {
-      vdem$episode2[i] <- 1
-      if (vdem$episode2[i - 1] == 1 & any(next.three == 1)) {
-        vdem$episode2[i] <- 1
-        if ((vdem$decline[i] == 0) & ((vdem$decline[i + 1] == 2 | vdem$decline[i + 2] == 2))) {
-          vdem$episode2[i] <- 0
-        }
-      }
-    }
+  if (vdem$episode[i] == 1 & vdem$episode[i - 1] == 0 & vdem$episode[i + 1] != 1)  {
+    dd <- vdem$v2x_polyarchy[i] - vdem$v2x_polyarchy[i - 1]
+    vdem$ep.diff[i] <- dd
   }
 }
 
-previous.three <- vdem$decline[i:(i - 3)]
-if (any(previous.three == 0) & vdem$episode2[i] == 2) {
-  vdem$episode2[i] <- 0
-}
-
-vdem$v8episode <- vdem8$ep3
-
-table(vdem$episode2[vdem$episode2 == 1])
-table(vdem8$ep3[vdem8$ep3 == 1])
-
-matches <- vdem[which(vdem$episode == 1), "country_name" ]
-matches1 <- vdem[which(vdem$episode == 1), "year" ]
-matches <- cbind(matches, matches1)
-
-matches.x <- vdem8[which(vdem8$ep3 == 1), "country_name" ]
-matches.xx <- vdem8[which(vdem8$ep3 == 1), "year" ]
-matches.x <- cbind(matches.x, matches.xx)
-
-setdiff(matches.x, matches)
-
-
-vdem$episode <- vdem$decline
+## Episode difference lag 2
 for (i in 2:nrow(vdem)) {
-  next.four <- vdem$decline[i:(i + 4)]
-  if (vdem$decline[i] == 0) {
-    if (vdem$decline[i - 1] == 1 & any(lag(vdem$cnext.four == 1)) {
-      vdem$episode[i] <- 1
-      if (vdem$decline[i - 1] == 2) {
-        vdem$episode[i] <- 0
-      } 
-    }
+  if (vdem$episode[i] == 1 & vdem$episode[i - 1] == 1 & vdem$episode[i + 1] != 1)  {
+    dd <- vdem$v2x_polyarchy[i] - vdem$v2x_polyarchy[i - 2]
+    vdem$ep.diff[i] <- dd
   }
 }
 
-if (vdem$decline[i] != 2) {
-  vdem$episode[i] <- 1
-  previous.year <- vdem$decline[i:(i - 1)]
-  
-vdem$episode[i] <- 1
-vdem$episode[i + 1] <- 1
-vdem$episode[i + 2] <- 1
-vdem$episode[i + 3] <- 1
-if (any(next.four == 2)) {
-  vdem$episode[i] <- 0
-}
-
-# trying to figure out +.02
-vdem$episode <- vdem$decline
+## Episode difference lag 3
 for (i in 2:nrow(vdem)) {
-  if (vdem$decline[i] == 0) {
-    if (vdem$decline[i - 1] == 1) {
-       next.three <- vdem$decline[i:(i +3)]
-       if (any(next.three == 1)) {
-         vdem$episode[i] <- 1
-         if (vdem$decline[i] == 2 & vdem$decline[i - 1] == 0) {
-           vdem$episode[i - 1] <- 0
-         }
-      } 
-    }
+  if (vdem$episode[i] == 1 & vdem$episode[i - 1] == 1 && vdem$episode[i - 2] == 1 & vdem$episode[i + 1] != 1)  {
+    dd <- vdem$v2x_polyarchy[i] - vdem$v2x_polyarchy[i - 3]
+    vdem$ep.diff[i] <- dd
   }
 }
 
-# episode2
-vdem$episode2 <- vdem$decline
+## Episode difference lag 4
 for (i in 2:nrow(vdem)) {
-  if (vdem$decline[i] == 0) {
-    if (vdem$decline[i - 1] == 1) {
-      next.three <- vdem$decline[i:(i +3)]
-      if (any(next.three == 1)) {
-        vdem$episode2[i] <- 1
-        if (any(next.three == 2)) {
-          vdem$episode2[i] <- 0
-        }
-      } 
-    }
+  if (vdem$episode[i] == 1 & vdem$episode[i - 1] == 1 && vdem$episode[i - 2] == 1 && vdem$episode[i - 3] == 1 & vdem$episode[i + 1] != 1)  {
+    dd <- vdem$v2x_polyarchy[i] - vdem$v2x_polyarchy[i - 4]
+    vdem$ep.diff[i] <- dd
   }
 }
 
-
-table(vdem$episode2[vdem$episode2 == 1])
-table(vdem8$ep3[vdem8$ep3 == 1])
-
-table(vdem$episode2[vdem$episode2 == 1 & vdem$diff > -.01])
-print(vdem[which(vdem$episode2 == 1 & vdem$diff > -.01), "year" ])
-
-
-
-# This worked!!!!
+## Episode difference lag 5
 for (i in 2:nrow(vdem)) {
-  if (vdem$decline[i] == 0) {
-    if (vdem$decline[i - 1] == 1) {
-      next.three <- vdem$decline[i:(i +3)]
-      if (any(next.three == 1)) {
-        vdem$episode[i] <- 1
-      } 
-    }
+  if (vdem$episode[i] == 1 & vdem$episode[i - 1] == 1 && vdem$episode[i - 2] == 1 && vdem$episode[i - 3] == 1 && vdem$episode[i - 4] == 1 & vdem$episode[i + 1] != 1)  {
+    dd <- vdem$v2x_polyarchy[i] - vdem$v2x_polyarchy[i - 5]
+    vdem$ep.diff[i] <- dd
+  }
+}
+
+## Episode difference lag 6
+for (i in 2:nrow(vdem)) {
+  if (vdem$episode[i] == 1 & vdem$episode[i - 1] == 1 && vdem$episode[i - 2] == 1 && vdem$episode[i - 3] == 1 && vdem$episode[i - 4] == 1 && vdem$episode[i - 5] == 1 & vdem$episode[i + 1] != 1)  {
+    dd <- vdem$v2x_polyarchy[i] - vdem$v2x_polyarchy[i - 6]
+    vdem$ep.diff[i] <- dd
+  }
+}
+
+## Episode difference lag 7
+for (i in 2:nrow(vdem)) {
+  if (vdem$episode[i] == 1 & vdem$episode[i - 1] == 1 && vdem$episode[i - 2] == 1 && vdem$episode[i - 3] == 1 && vdem$episode[i - 4] == 1 && vdem$episode[i - 5] == 1 && vdem$episode[i - 6] == 1 & vdem$episode[i + 1] != 1)  {
+    dd <- vdem$v2x_polyarchy[i] - vdem$v2x_polyarchy[i - 7]
+    vdem$ep.diff[i] <- dd
+  }
+}
+
+## Episode difference lag 8
+for (i in 2:nrow(vdem)) {
+  if (vdem$episode[i] == 1 & vdem$episode[i - 1] == 1 && vdem$episode[i - 2] == 1 && vdem$episode[i - 3] == 1 && vdem$episode[i - 4] == 1 && vdem$episode[i - 5] == 1 && vdem$episode[i - 6] == 1 && vdem$episode[i - 7] == 1 & vdem$episode[i + 1] != 1)  {
+    dd <- vdem$v2x_polyarchy[i] - vdem$v2x_polyarchy[i - 8]
+    vdem$ep.diff[i] <- dd
+  }
+}
+
+## Episode difference lag 9
+for (i in 2:nrow(vdem)) {
+  if (vdem$episode[i] == 1 & vdem$episode[i - 1] == 1 && vdem$episode[i - 2] == 1 && vdem$episode[i - 3] == 1 && vdem$episode[i - 4] == 1 && vdem$episode[i - 5] == 1 && vdem$episode[i - 6] == 1 && vdem$episode[i - 7] == 1 && vdem$episode[i - 8] == 1 & vdem$episode[i + 1] != 1)  {
+    dd <- vdem$v2x_polyarchy[i] - vdem$v2x_polyarchy[i - 9]
+    vdem$ep.diff[i] <- dd
+  }
+}
+
+## Episode difference lag 10
+for (i in 2:nrow(vdem)) {
+  if (vdem$episode[i] == 1 & vdem$episode[i - 1] == 1 && vdem$episode[i - 2] == 1 && vdem$episode[i - 3] == 1 && vdem$episode[i - 4] == 1 && vdem$episode[i - 5] == 1 && vdem$episode[i - 6] == 1 && vdem$episode[i - 7] == 1 && vdem$episode[i - 8] == 1 && vdem$episode[i - 9] == 1 & vdem$episode[i + 1] != 1)  {
+    dd <- vdem$v2x_polyarchy[i] - vdem$v2x_polyarchy[i - 10]
+    vdem$ep.diff[i] <- dd
   }
 }
 
 
 
-for (i in 2:nrow(vdem)) {
-  if (vdem$decline[i] == 1) {
-    print("hello")
-      } 
-    }
 
-  for (i in 1:dim(vdem)[1]) {
-    if (vdem$decline[i] == 1) {
-      vdem$example[i] <- vdem$example[i]*5
-    } 
-  }
-  
-vdem$example <- vdem$diff
-for (i in 1:dim(vdem)[1]) {
-  if (vdem$decline[i] == 1) {
-    vdem$example[i] <- vdem$example[i]*5
-  } 
+
+
+
+###### Creating function
+
+backslide(vdemdata = vdem, x = v2x_polyarchy, group = country_id, new.column = "Episodes")
+
+backslide <- function(data, x, new.column) {
+  data <- as.data.frame(data)
+  x <- data$x
+  new.column <- length(nrow(data))
+  data <- data %>% group_by(country_id) %>% mutate(new.column = x - lag(x))
 }
 
-
-# any() looks at vector of trues and falses and returns any if they're true
-
+backslide(data = vdem, x = v2x_polyarchy, new.column = "diff")
 
 
+# Trying again, without "new.column" argument
+backslide <- function(data, x) {
+  data <- as.data.frame(data)
+  x <- data$x
+  data <- data %>% group_by(country_id) %>% mutate(diff = x - lag(x))
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+backslide(data = vdem, x = "v2x_polyarchy")
--- project draft.R
+++ project draft.R
@@ -142,3 +142,314 @@ vdem8a <- subset(vdem8a, ep.change <= -.1)
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+rm(list=ls())
+
+library(dplyr)
+
+vdem.full <- read.csv("~/Desktop/data_2018F/V-Dem-CY-Core-v8.csv")
+
+## For comparison
+
+vdem8 <- subset(vdem.full, year > 1959 & year < 2000, 
+                select = c(country_name, country_text_id, country_id, year, v2x_polyarchy))
+
+# Creating new variable if decline from year to year was greater than -.01
+vdem8 <- vdem8 %>% 
+  group_by(country_id) %>%
+  mutate(dec01 = v2x_polyarchy - lag(v2x_polyarchy) <= -0.01)
+vdem8$dec01 <- as.numeric(vdem8$dec01)
+
+# Creating new variable of v2x_polyarchy (EDI) difference from year to year
+vdem8 <- vdem8 %>% 
+  group_by(country_id) %>%
+  mutate(diff = v2x_polyarchy - lag(v2x_polyarchy))
+
+# Creating new diff column with more decimals for easier viewing
+vdem8$diff2 <- 0
+vdem8[,"diff2"] <- round(vdem8[,'diff'],10)
+
+# Coding observations as 1's for declines of 0.01, including 4-year stagnation period
+# If no EDI 0.01 declines over 4 years, or if EDI increases by 0.02, then observation is coded as 0
+
+# Step 1
+vdem8 <- vdem8 %>% 
+  group_by(country_id) %>%
+  mutate(ep1 = case_when( (dec01 == 0 & lag(dec01) == 1 & lead(dec01) == 1) & (diff < .02) ~ 1,
+                          dec01 == 1 ~ 1,
+                          TRUE ~ 0))
+# Step 2
+vdem8 <- vdem8 %>% 
+  group_by(country_id) %>%
+  mutate(ep2 = case_when( (dec01 == 0 & lag(dec01) == 1 & lead(dec01) == 1) & (diff < .02) ~ 1,
+                          (dec01 == 0 & lag(dec01,2) == 1 & (lead(dec01) == 1 | lead(dec01,2) == 1)) & (diff < .02 & lag(diff) < .02) ~ 1,
+                          (dec01 == 0 & lag(dec01) == 1 & lead(dec01,2) == 1) & (diff < .02) ~ 1,
+                          dec01 == 1 ~ 1,
+                          TRUE ~ 0))
+# Step 3
+vdem8 <- vdem8 %>% 
+  group_by(country_id) %>%
+  mutate(ep3 = case_when( (ep2 == 0 & lag(ep2) == 1 & lead(ep2) == 1) & (diff < .02) ~ 1,
+                          ep2 == 1 ~ 1,
+                          TRUE ~ 0))
+
+
+
+
+
+# Subsetting V-dem data
+vdem <- subset(vdem.full, year > 1959 & year < 2000, 
+               select = c(country_name, country_text_id, country_id, year, v2x_polyarchy))
+
+
+# Change in v2x_polyarchy from one year to the next
+vdem <- vdem %>% 
+  group_by(country_id) %>%
+  mutate(diff = v2x_polyarchy - lag(v2x_polyarchy))
+
+# New diff column for more decimals
+vdem$diff2 <- 0
+vdem[,"diff2"] <- round(vdem[,'diff'],10)
+
+vdem$diff3 <- 0
+vdem$diff3[is.na(vdem$diff)] <- 99
+
+# New variable for basic 0.01 threshold
+#  vdem <- vdem %>% 
+#    group_by(country_id) %>%
+#    mutate(decline2 = case_when(diff <= -0.01 ~ 1,
+#                               TRUE ~ 0))
+
+# New variable: 1 = decline greater/equal to .01, 2 = incline greater/equal to .02, 0 = all else
+vdem <- vdem %>% 
+  group_by(country_id) %>%
+  mutate(decline = case_when(diff <= -0.01 ~ 1,
+                             diff >= 0.02 ~ 2,
+                             TRUE ~ 0))
+
+
+
+# Think this is right, but need to check
+vdem$episode2 <- vdem$decline
+for (i in 2:nrow(vdem)) {
+  next.three <- vdem$decline[i:(i + 3)]
+  if (vdem$episode2[i] == 0) {
+    if (vdem$episode2[i - 1] == 1 & any(next.three == 1)) {
+      vdem$episode2[i] <- 1
+      if (vdem$episode2[i - 1] == 1 & any(next.three == 1)) {
+        vdem$episode2[i] <- 1
+        if ((vdem$decline[i] == 0) & (((vdem$decline[i + 1] == 2 | vdem$decline[i + 2] == 2) & (vdem$decline[i + 1] != 1) ) ) ) {
+          vdem$episode2[i] <- 0
+        }
+      }
+    }
+  }
+}
+
+# Replaces 1's with 0's for first year per country
+vdem$episode2[is.na(vdem$diff)] <- 0
+
+# Replace 2's with 0's
+vdem$episode2[vdem$episode2 == 2] <- 0
+
+
+for (i in 2:nrow(vdem)) {
+  if (vdem$episode2[i] == 0) {
+    if (vdem$episode2[i - 1] == 1 & any(next.three == 1)) {
+      vdem$episode2[i] <- 1
+      if (vdem$episode2[i - 1] == 1 & any(next.three == 1)) {
+        vdem$episode2[i] <- 1
+        if ((vdem$decline[i] == 0) & (((vdem$decline[i + 1] == 2 | vdem$decline[i + 2] == 2) & (vdem$decline[i + 1] != 1) ) ) ) {
+          vdem$episode2[i] <- 0
+        }
+      }
+    }
+  }
+}
+
+
+###### Creating function
+
+backslide(vdem, v2x_polyarchy, new.column = "Episodes")
+
+backslide <- function(vdemdata, x, group, new.column) {
+  
+  vdem <- vdem %>% 
+    group_by(country_id) %>%
+    mutate(diff = x - lag(x))
+  
+  
+  
+}
+
+# Change in v2x_polyarchy from one year to the next
+vdem <- vdem %>% 
+  group_by(country_id) %>%
+  mutate(diff = v2x_polyarchy - lag(v2x_polyarchy))
+
+# New diff column for more decimals
+vdem$diff2 <- 0
+vdem[,"diff2"] <- round(vdem[,'diff'],10)
+
+vdem$diff3 <- 0
+vdem$diff3[is.na(vdem$diff)] <- 99
+
+# New variable for basic 0.01 threshold
+#  vdem <- vdem %>% 
+#    group_by(country_id) %>%
+#    mutate(decline2 = case_when(diff <= -0.01 ~ 1,
+#                               TRUE ~ 0))
+
+# New variable: 1 = decline greater/equal to .01, 2 = incline greater/equal to .02, 0 = all else
+vdem <- vdem %>% 
+  group_by(country_id) %>%
+  mutate(decline = case_when(diff <= -0.01 ~ 1,
+                             diff >= 0.02 ~ 2,
+                             TRUE ~ 0))
+
+
+# Not quite right
+vdem$episode <- vdem$decline
+for (i in 2:nrow(vdem)) {
+  next.three <- vdem$episode[i:(i + 3)]
+  if (vdem$episode[i] == 0) {
+    if (vdem$episode[i - 1] == 1 & any(next.three == 1)) {
+      vdem$episode[i] <- 1
+      if (vdem$episode[i - 1] == 1 & any(next.three == 1)) {
+        vdem$episode[i] <- 1
+        if ((vdem$decline[i] == 0) & (vdem$decline[i + 1] == 2)) {
+          vdem$episode[i] <- 0
+        }
+      }
+    }
+  }
+}
+
+# Think this is right, but need to check
+vdem$episode2 <- vdem$decline
+for (i in 2:nrow(vdem)) {
+  next.three <- vdem$decline[i:(i + 3)]
+  if (vdem$episode2[i] == 0) {
+    if (vdem$episode2[i - 1] == 1 & any(next.three == 1)) {
+      vdem$episode2[i] <- 1
+      if (vdem$episode2[i - 1] == 1 & any(next.three == 1)) {
+        vdem$episode2[i] <- 1
+        if ((vdem$decline[i] == 0) & (((vdem$decline[i + 1] == 2 | vdem$decline[i + 2] == 2) & (vdem$decline[i + 1] != 1) ) ) ) {
+          vdem$episode2[i] <- 0
+        }
+      }
+    }
+  }
+}
+
+# Replaces 1's with 0's for first year per country
+vdem$episode2[is.na(vdem$diff)] <- 0
+
+
+
+# adding ep3 variable 
+# vdem$v8episode <- vdem8$ep3
+
+
+
+
+
+
+
+
+
+
+table(vdem$episode2[vdem$episode2 == 1])
+table(vdem8$ep3[vdem8$ep3 == 1])
+table(vdem$v8episode[vdem$v8episode == 1])
+
+matches <- vdem[which(vdem$episode2 == 1), "country_name" ]
+matches1 <- vdem[which(vdem$episode2 == 1), "year" ]
+matches <- cbind(matches, matches1)
+
+matches.x <- vdem8[which(vdem8$ep3 == 1), "country_name" ]
+matches.xx <- vdem8[which(vdem8$ep3 == 1), "year" ]
+matches.x <- cbind(matches.x, matches.xx)
+
+setdiff(matches.x, matches)
+setdiff(matches, matches.x)
+
+
+
+
+
+
+table(vdem$episode2[vdem$episode2 == 1 & vdem$diff > -.01])
+print(vdem[which(vdem$episode2 == 1 & vdem$diff > -.01), "year" ])
+
+
+
+# This worked!!!!
+for (i in 2:nrow(vdem)) {
+  if (vdem$decline[i] == 0) {
+    if (vdem$decline[i - 1] == 1) {
+      next.three <- vdem$decline[i:(i +3)]
+      if (any(next.three == 1)) {
+        vdem$episode[i] <- 1
+      } 
+    }
+  }
+}
+
+
+
+
+
+
+
+
+# Not quite right
+vdem$episode <- vdem$decline
+for (i in 2:nrow(vdem)) {
+  next.three <- vdem$episode[i:(i + 3)]
+  if (vdem$episode[i] == 0) {
+    if (vdem$episode[i - 1] == 1 & any(next.three == 1)) {
+      vdem$episode[i] <- 1
+      if (vdem$episode[i - 1] == 1 & any(next.three == 1)) {
+        vdem$episode[i] <- 1
+        if ((vdem$decline[i] == 0) & (vdem$decline[i + 1] == 2)) {
+          vdem$episode[i] <- 0
+        }
+      }
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
